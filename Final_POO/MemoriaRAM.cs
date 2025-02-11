f799179e2d3bd6d2e2fdd59db57e837f4754443e
Pacha <jaimelucas2001@gmail.com>
2025-02-11 15:00:50 -0300

    cree la clase MemoriaRam

---
+++ Final_POO/MemoriaRAM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Final_POO;
+
+namespace Final_POO
+{
+    public class MemoriaRAM : ProductoInformatico
+    {
+        public int CapacidadGB { get; set; }
+
+        public MemoriaRAM(string nombre, string marca, decimal precio, int stock, int capacidadGB)
+            : base(nombre, marca, precio, stock, "Memoria RAM")
+        {
+            CapacidadGB = capacidadGB;
+        }
+
+        public override decimal CalcularDescuento(decimal porcentaje)
+        {
+            return Precio - (Precio * (porcentaje / 100));
+        }
+    }
+}
--- Final_POO/ProductoInformatico.cs
+++ Final_POO/ProductoInformatico.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace VentaProductos
+namespace Final_POO
 {
     public abstract class ProductoInformatico
     {
