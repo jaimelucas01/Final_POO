d25ffe05206f91c1d308215982e89d3e86eb6d68
tomardo <iamtomasorlandoni@gmail.com>
2025-02-13 11:13:29 -0300

    Titulo de Aplicación

--- Final_POO/Form1.Designer.cs
+++ Final_POO/Form1.Designer.cs
@@ -140,7 +140,7 @@
             Controls.Add(label1);
             Controls.Add(dataGridView1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Final POO";
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
