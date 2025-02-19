95bc604c796f30fbe88f341aceca52ed1ad569e4
tomardo <iamtomasorlandoni@gmail.com>
2025-02-19 15:42:34 -0300

    Nuevos cambios en el Diseño

--- Final_POO/Form1.Designer.cs
+++ Final_POO/Form1.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             dgvPartes = new DataGridView();
+            Marca = new DataGridViewTextBoxColumn();
+            Componente = new DataGridViewTextBoxColumn();
+            Tipo = new DataGridViewTextBoxColumn();
+            Stock = new DataGridViewTextBoxColumn();
+            Precio = new DataGridViewTextBoxColumn();
             label1 = new Label();
             txtMarca = new TextBox();
             txtComp = new TextBox();
@@ -43,143 +48,23 @@
             txtTipo = new TextBox();
             label5 = new Label();
             btnExit = new Button();
-            Marca = new DataGridViewTextBoxColumn();
-            Componente = new DataGridViewTextBoxColumn();
-            Tipo = new DataGridViewTextBoxColumn();
-            Stock = new DataGridViewTextBoxColumn();
-            Precio = new DataGridViewTextBoxColumn();
+            gbo1 = new GroupBox();
+            gbo2 = new GroupBox();
+            btnGuardar = new Button();
             ((System.ComponentModel.ISupportInitialize)dgvPartes).BeginInit();
+            gbo1.SuspendLayout();
+            gbo2.SuspendLayout();
             SuspendLayout();
             // 
             // dgvPartes
             // 
             dgvPartes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dgvPartes.Columns.AddRange(new DataGridViewColumn[] { Marca, Componente, Tipo, Stock, Precio });
-            dgvPartes.Location = new Point(12, 12);
+            dgvPartes.Location = new Point(12, 22);
             dgvPartes.Name = "dgvPartes";
             dgvPartes.Size = new Size(646, 337);
             dgvPartes.TabIndex = 0;
             dgvPartes.CellClick += dgvPartes_CellClick;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(676, 12);
-            label1.Name = "label1";
-            label1.Size = new Size(40, 15);
-            label1.TabIndex = 1;
-            label1.Text = "Marca";
-            // 
-            // txtMarca
-            // 
-            txtMarca.Location = new Point(676, 30);
-            txtMarca.Name = "txtMarca";
-            txtMarca.Size = new Size(230, 23);
-            txtMarca.TabIndex = 3;
-            // 
-            // txtComp
-            // 
-            txtComp.Location = new Point(676, 74);
-            txtComp.Name = "txtComp";
-            txtComp.Size = new Size(230, 23);
-            txtComp.TabIndex = 5;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(676, 56);
-            label2.Name = "label2";
-            label2.Size = new Size(77, 15);
-            label2.TabIndex = 4;
-            label2.Text = "Componente";
-            // 
-            // txtPrecio
-            // 
-            txtPrecio.Location = new Point(676, 206);
-            txtPrecio.Name = "txtPrecio";
-            txtPrecio.Size = new Size(230, 23);
-            txtPrecio.TabIndex = 7;
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(676, 188);
-            label3.Name = "label3";
-            label3.Size = new Size(40, 15);
-            label3.TabIndex = 6;
-            label3.Text = "Precio";
-            // 
-            // btnAdd
-            // 
-            btnAdd.Location = new Point(676, 235);
-            btnAdd.Name = "btnAdd";
-            btnAdd.Size = new Size(230, 41);
-            btnAdd.TabIndex = 8;
-            btnAdd.Text = "Agregar";
-            btnAdd.UseVisualStyleBackColor = true;
-            btnAdd.Click += btnAdd_Click;
-            // 
-            // btnMod
-            // 
-            btnMod.Location = new Point(12, 355);
-            btnMod.Name = "btnMod";
-            btnMod.Size = new Size(107, 39);
-            btnMod.TabIndex = 9;
-            btnMod.Text = "Modificar";
-            btnMod.UseVisualStyleBackColor = true;
-            btnMod.Click += btnMod_Click;
-            // 
-            // btnDel
-            // 
-            btnDel.Location = new Point(125, 355);
-            btnDel.Name = "btnDel";
-            btnDel.Size = new Size(107, 39);
-            btnDel.TabIndex = 10;
-            btnDel.Text = "Eliminar";
-            btnDel.UseVisualStyleBackColor = true;
-            btnDel.Click += btnDel_Click;
-            // 
-            // txtStock
-            // 
-            txtStock.Location = new Point(676, 162);
-            txtStock.Name = "txtStock";
-            txtStock.Size = new Size(230, 23);
-            txtStock.TabIndex = 12;
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(676, 144);
-            label4.Name = "label4";
-            label4.Size = new Size(36, 15);
-            label4.TabIndex = 11;
-            label4.Text = "Stock";
-            // 
-            // txtTipo
-            // 
-            txtTipo.Location = new Point(676, 118);
-            txtTipo.Name = "txtTipo";
-            txtTipo.Size = new Size(230, 23);
-            txtTipo.TabIndex = 14;
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(676, 100);
-            label5.Name = "label5";
-            label5.Size = new Size(30, 15);
-            label5.TabIndex = 13;
-            label5.Text = "Tipo";
-            // 
-            // btnExit
-            // 
-            btnExit.Location = new Point(801, 390);
-            btnExit.Name = "btnExit";
-            btnExit.Size = new Size(107, 39);
-            btnExit.TabIndex = 15;
-            btnExit.Text = "Salir";
-            btnExit.UseVisualStyleBackColor = true;
-            btnExit.Click += btnExit_Click;
             // 
             // Marca
             // 
@@ -211,31 +96,183 @@
             Precio.HeaderText = "Precio";
             Precio.Name = "Precio";
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(40, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Marca";
+            // 
+            // txtMarca
+            // 
+            txtMarca.Location = new Point(12, 40);
+            txtMarca.Name = "txtMarca";
+            txtMarca.Size = new Size(230, 23);
+            txtMarca.TabIndex = 3;
+            // 
+            // txtComp
+            // 
+            txtComp.Location = new Point(12, 84);
+            txtComp.Name = "txtComp";
+            txtComp.Size = new Size(230, 23);
+            txtComp.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 66);
+            label2.Name = "label2";
+            label2.Size = new Size(77, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Componente";
+            // 
+            // txtPrecio
+            // 
+            txtPrecio.Location = new Point(12, 216);
+            txtPrecio.Name = "txtPrecio";
+            txtPrecio.Size = new Size(230, 23);
+            txtPrecio.TabIndex = 7;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 198);
+            label3.Name = "label3";
+            label3.Size = new Size(40, 15);
+            label3.TabIndex = 6;
+            label3.Text = "Precio";
+            // 
+            // btnAdd
+            // 
+            btnAdd.Location = new Point(16, 364);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(107, 41);
+            btnAdd.TabIndex = 8;
+            btnAdd.Text = "Agregar";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += btnAdd_Click;
+            // 
+            // btnMod
+            // 
+            btnMod.Location = new Point(129, 364);
+            btnMod.Name = "btnMod";
+            btnMod.Size = new Size(107, 39);
+            btnMod.TabIndex = 9;
+            btnMod.Text = "Modificar";
+            btnMod.UseVisualStyleBackColor = true;
+            btnMod.Click += btnMod_Click;
+            // 
+            // btnDel
+            // 
+            btnDel.Location = new Point(242, 364);
+            btnDel.Name = "btnDel";
+            btnDel.Size = new Size(107, 39);
+            btnDel.TabIndex = 10;
+            btnDel.Text = "Eliminar";
+            btnDel.UseVisualStyleBackColor = true;
+            btnDel.Click += btnDel_Click;
+            // 
+            // txtStock
+            // 
+            txtStock.Location = new Point(12, 172);
+            txtStock.Name = "txtStock";
+            txtStock.Size = new Size(230, 23);
+            txtStock.TabIndex = 12;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 154);
+            label4.Name = "label4";
+            label4.Size = new Size(36, 15);
+            label4.TabIndex = 11;
+            label4.Text = "Stock";
+            // 
+            // txtTipo
+            // 
+            txtTipo.Location = new Point(12, 128);
+            txtTipo.Name = "txtTipo";
+            txtTipo.Size = new Size(230, 23);
+            txtTipo.TabIndex = 14;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(12, 110);
+            label5.Name = "label5";
+            label5.Size = new Size(30, 15);
+            label5.TabIndex = 13;
+            label5.Text = "Tipo";
+            // 
+            // btnExit
+            // 
+            btnExit.Location = new Point(143, 394);
+            btnExit.Name = "btnExit";
+            btnExit.Size = new Size(107, 39);
+            btnExit.TabIndex = 15;
+            btnExit.Text = "Salir";
+            btnExit.UseVisualStyleBackColor = true;
+            btnExit.Click += btnExit_Click;
+            // 
+            // gbo1
+            // 
+            gbo1.Controls.Add(dgvPartes);
+            gbo1.Controls.Add(btnMod);
+            gbo1.Controls.Add(btnAdd);
+            gbo1.Controls.Add(btnDel);
+            gbo1.Location = new Point(0, 0);
+            gbo1.Name = "gbo1";
+            gbo1.Size = new Size(670, 439);
+            gbo1.TabIndex = 16;
+            gbo1.TabStop = false;
+            gbo1.Text = "Lista";
+            // 
+            // gbo2
+            // 
+            gbo2.Controls.Add(btnGuardar);
+            gbo2.Controls.Add(btnExit);
+            gbo2.Controls.Add(txtTipo);
+            gbo2.Controls.Add(label1);
+            gbo2.Controls.Add(label5);
+            gbo2.Controls.Add(txtMarca);
+            gbo2.Controls.Add(txtStock);
+            gbo2.Controls.Add(label2);
+            gbo2.Controls.Add(label4);
+            gbo2.Controls.Add(txtComp);
+            gbo2.Controls.Add(label3);
+            gbo2.Controls.Add(txtPrecio);
+            gbo2.Location = new Point(665, 0);
+            gbo2.Name = "gbo2";
+            gbo2.Size = new Size(256, 439);
+            gbo2.TabIndex = 17;
+            gbo2.TabStop = false;
+            gbo2.Text = "Ingreso";
+            // 
+            // btnGuardar
+            // 
+            btnGuardar.Location = new Point(11, 245);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(230, 41);
+            btnGuardar.TabIndex = 16;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(920, 441);
-            Controls.Add(btnExit);
-            Controls.Add(txtTipo);
-            Controls.Add(label5);
-            Controls.Add(txtStock);
-            Controls.Add(label4);
-            Controls.Add(btnDel);
-            Controls.Add(btnMod);
-            Controls.Add(btnAdd);
-            Controls.Add(txtPrecio);
-            Controls.Add(label3);
-            Controls.Add(txtComp);
-            Controls.Add(label2);
-            Controls.Add(txtMarca);
-            Controls.Add(label1);
-            Controls.Add(dgvPartes);
+            Controls.Add(gbo1);
+            Controls.Add(gbo2);
             Name = "Form1";
             Text = "Final POO";
             ((System.ComponentModel.ISupportInitialize)dgvPartes).EndInit();
+            gbo1.ResumeLayout(false);
+            gbo2.ResumeLayout(false);
+            gbo2.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -260,5 +297,8 @@
         private DataGridViewTextBoxColumn Tipo;
         private DataGridViewTextBoxColumn Stock;
         private DataGridViewTextBoxColumn Precio;
+        private GroupBox gbo1;
+        private GroupBox gbo2;
+        private Button btnGuardar;
     }
 }
